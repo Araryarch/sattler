96858e75e126a51e0515d2f945c9c08146667666
Araryarch <Araryaarch@gmail.com>
2024-08-05 10:59:32 +0700

    update Desgin.tsx

--- src/pages/Desgin.tsx
+++ src/pages/Desgin.tsx
@@ -8,7 +8,8 @@ const Design = () => {
     <div
       className={`${inter.className} containers relative w-full min-h-screen bg-latte-mauve`}
     >
-      <nav className='w-full p-5 flex justify-between'>
+      {/* Navbar */}
+      <nav className='w-full p-5 flex justify-between fixed backdrop-blur-sm'>
         <div className='logo'>
           <h1 className='font-bold text-2xl text-latte-rosewater'>
             ELLIE SATTLER
@@ -20,6 +21,14 @@ const Design = () => {
           </h1>
         </div>
       </nav>
+
+      {/* Main */}
+      <div className='div w-full min-h-screen flex justify-center items-start flex-col p-5 gap-3'>
+        <h1 className='text-8xl text-latte-pink'>Kompak Bersatu,</h1>
+        <h2 className='text-4xl text-latte-maroon'>Semangat Tak Luntur.</h2>
+      </div>
+
+      {/* Botbar */}
     </div>
   )
 }
