d3d64d209f10299da7800088e921a92472b4bf89
Araryarch <Araryaarch@gmail.com>
2024-08-05 11:04:54 +0700

    update Desgin.tsx

--- src/pages/Desgin.tsx
+++ src/pages/Desgin.tsx
@@ -11,10 +11,12 @@ const Design = () => {
       {/* Navbar */}
       <nav className='w-full p-5 flex justify-between fixed backdrop-blur-sm border-b-2 border-latte-mauve'>
         <div className='logo'>
-          <h1 className='font-bold text-2xl text-latte-text'>ELLIE SATTLER</h1>
+          <h1 className='font-bold text-md xl:text-2xl text-latte-text'>
+            ELLIE SATTLER
+          </h1>
         </div>
         <div className='desc'>
-          <h1 className='font-bold text-xl text-latte-text'>
+          <h1 className='font-bold text-sm xl:text-xl text-latte-text'>
             UNIT 2 - Jurrasic Park
           </h1>
         </div>
@@ -22,8 +24,10 @@ const Design = () => {
 
       {/* Main */}
       <div className='div w-full min-h-screen flex justify-center items-start flex-col p-5 gap-3'>
-        <h1 className='text-8xl text-latte-mauve font-bold'>Kompak Bersatu</h1>
-        <h2 className='text-4xl text-latte-sky font-medium'>
+        <h1 className='text-4xl xl:text-8xl text-latte-mauve font-bold'>
+          Kompak Bersatu
+        </h1>
+        <h2 className='text-2xl xl:text-4xl text-latte-sky font-medium'>
           Semangat Tak Luntur.
         </h2>
       </div>
