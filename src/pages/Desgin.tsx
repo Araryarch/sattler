9d4641105656486bc8fec1b17c1e36fe266e650e
Araryarch <Araryaarch@gmail.com>
2024-08-05 11:03:54 +0700

    update Desgin.tsx

--- src/pages/Desgin.tsx
+++ src/pages/Desgin.tsx
@@ -6,17 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 const Design = () => {
   return (
     <div
-      className={`${inter.className} containers relative w-full min-h-screen bg-latte-mauve`}
+      className={`${inter.className} containers relative w-full min-h-screen bg-latte-base`}
     >
       {/* Navbar */}
-      <nav className='w-full p-5 flex justify-between fixed backdrop-blur-sm'>
+      <nav className='w-full p-5 flex justify-between fixed backdrop-blur-sm border-b-2 border-latte-mauve'>
         <div className='logo'>
-          <h1 className='font-bold text-2xl text-latte-rosewater'>
-            ELLIE SATTLER
-          </h1>
+          <h1 className='font-bold text-2xl text-latte-text'>ELLIE SATTLER</h1>
         </div>
         <div className='desc'>
-          <h1 className='font-bold text-xl text-latte-rosewater'>
+          <h1 className='font-bold text-xl text-latte-text'>
             UNIT 2 - Jurrasic Park
           </h1>
         </div>
@@ -24,8 +22,10 @@ const Design = () => {
 
       {/* Main */}
       <div className='div w-full min-h-screen flex justify-center items-start flex-col p-5 gap-3'>
-        <h1 className='text-8xl text-latte-pink'>Kompak Bersatu,</h1>
-        <h2 className='text-4xl text-latte-maroon'>Semangat Tak Luntur.</h2>
+        <h1 className='text-8xl text-latte-mauve font-bold'>Kompak Bersatu</h1>
+        <h2 className='text-4xl text-latte-sky font-medium'>
+          Semangat Tak Luntur.
+        </h2>
       </div>
 
       {/* Botbar */}
